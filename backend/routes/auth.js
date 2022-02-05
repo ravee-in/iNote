16ae4de7045a390740d7d442f0068a704d87dc79
Ravi Kumar <connect@ravee.in>
2022-02-05 20:22:46 +0530

    5Feb22 v52#49

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs/dist/bcrypt');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "iNoteSAFEkey009";
+
 
 // Create a User using: POST "/api/auth/createuser". No Login required
 router.post('/createuser', [
@@ -21,21 +26,37 @@ router.post('/createuser', [
         return res.status(400).json({ errors: errors.array() });
     }
 
-
-
-    // Creating User in the DB || Check wether user exist with same email
     try {
+
+       // Creating User in the DB || Check wether user exist with same email 
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ error: "Sorry! This email is already used." })
         }
 
+        // Using bcryptjs to add Hashing Salt to password
+
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-        })
-        res.json(user)
+            password: secPass,
+        });
+
+        const data = {
+            user:{
+               id: user.id,
+            }
+        }
+
+        // adding Authentication Token using JWT
+        
+        const authToken = jwt.sign(data, JWT_SECRET);
+        console.log(authToken);
+
+        res.json({authToken})
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Some error occured")
