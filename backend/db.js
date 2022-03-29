23845f80879a89c69e1983281e80230b2eb5ebe0
Ravi Kumar <98800261+ravee-in@users.noreply.github.com>
2022-03-29 13:48:18 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-// const mongoURI = "mongodb+srv://ravee_in:raveee1234@inote001.6yapf.mongodb.net/test";
-const mongoURI = "mongodb://localhost:27017/inote?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoURI = "MongoDB URI";
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURI, ()=>{
